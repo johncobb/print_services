31eea4d5c6b998a2cc32cd989c0f25d9aedcb153
Evan Higgins <evanhiggins@Evans-MacBook-Pro.local>
2016-08-25 13:04:05 -0500

    Fixed logging to change file name daily, added production API

--- cpprinter/cpdefs.py
+++ cpprinter/cpdefs.py
@@ -4,11 +4,11 @@ class HttpCodes:
 
 class CpDefs:
     PrinterBaud = 9600
-    API_URL = "http://10.0.0.130/api/printer/getprintjob/"
+    API_URL = "http://unison.cphandheld.com/api/printer/getprintjob/"
     MESSAGE_CHECK_DELAY_S = 3
     DEBUG = True
 
 class CpLoggerConfig:
     LOG_DIRECTORY = "../logs/"
-    FILE_FORMAT_STR = "%H%M_%d_%m_%Y.log"
+    FILE_FORMAT_STR = "%d_%m_%Y.log"
     LOG_VERBOSE = True
