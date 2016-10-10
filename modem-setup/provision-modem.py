bca993cbf0923247275bbbc591b35d5268b66525
Evan Higgins <evanhiggins@Evans-MacBook-Pro-2.local>
2016-10-10 13:55:48 -0500

    Added remote admin setup

--- modem-setup/provision-modem.py
+++ modem-setup/provision-modem.py
@@ -6,14 +6,17 @@ NEW_PASSWORD_HASH = '"blah"'
 
 def getCommands():
     return [
-        #'set /config/system/users/0/password ' + NEW_PASSWORD_HASH,
         'set /config/system/timezone "+6"',
 
         'set /config/firewall/macfilter/enabled true',
 
-        'set /config/dhcpd/reserve [{"ip6_address": null, "hostname":"raspberrypi", "ip_address":"192.168.0.2", "mac":"' + getPiMacAddress() + '", "duid":"01:b8:27:eb:fb:ca:79"}]'
+        'set /config/dhcpd/reserve [{"ip6_address": null, "hostname":"raspberrypi", "ip_address":"192.168.0.2", "mac":"' + getPiMacAddress() + '", "duid":"01:b8:27:eb:fb:ca:79"}]',
 
-        'set /config/firewall/portfwd [{"enabled": true, "ip_address": "192.168.0.2", "lan_port_offt": 22, "name": "PiPrinter", "protocol": "both", "wan_port_end": 2022, "wan_port_start":2022}]'
+        'set /config/firewall/portfwd [{"enabled": true, "ip_address": "192.168.0.2", "lan_port_offt": 22, "name": "PiPrinter", "protocol": "both", "wan_port_end": 2022, "wan_port_start":2022}]',
+
+        'set /config/firewall/remote_admin/enabled {"enabled":true, "port":8080, "restrict_ips":false, "secure_only":true, "secure_port":8443, "usb_logging":false, "allowed_ips":[]}'
+
+        #'set /config/system/users/0/password ' + NEW_PASSWORD_HASH,
     ]
 
 def main():
