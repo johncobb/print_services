dfcf32a14692466e549210c26d7ff31119f65a30
Evan Higgins <evanhiggins@Evans-MacBook-Pro-2.local>
2016-10-10 12:35:08 -0500

    Attempt to change modem password

--- modem-setup/provision-modem.sh
+++ modem-setup/provision-modem.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+MODEM_PASSWORD=`./get-modem-pass.py`
 
-
+sshpass -p $MODEM_PASSWORD ssh -oStrictHostKeyChecking=no admin@cp "set /config/system/users/0/password \$1\$19dea550\$rlEEoW3nT29UXyfY/tuUEg=="
 ########## DHCP Reservation ##########
