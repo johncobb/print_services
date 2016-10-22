1dfa545176dc3a677d3441882a65422b6e6ee99a
Evan Higgins <evan.higgins06@gmail.com>
2016-10-22 00:21:50 +0000

    Added code to write to socket. Untested

--- cpprinter-rust/src/main.rs
+++ cpprinter-rust/src/main.rs
@@ -1,5 +1,11 @@
+extern crate serial;
+
 use std::net::{TcpListener, TcpStream};
 use std::io::Read;
+use std::io;
+
+use serial::prelude::*;
+use std::io::prelude::Write;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:5555").unwrap();
@@ -22,6 +28,19 @@ fn handle_connection(stream: &mut TcpStream) {
     print_label(String::from_utf8(label).unwrap());
 }
 
-fn print_label(label: String) {
+fn print_label(label: String) -> io::Result<()> {
+    let mut port = serial::open("/dev/ttyUSB0").unwrap();
+    try!(port.reconfigure(&|settings| {
+        try!(settings.set_baud_rate(serial::Baud9600));
+        settings.set_char_size(serial::Bits8);
+        settings.set_parity(serial::ParityNone);
+        settings.set_stop_bits(serial::Stop1);
+        settings.set_flow_control(serial::FlowNone);
+        Ok(())
+    }));
+
+    try!(port.write(label.as_bytes()));
+
     println!("{}", label);
+    return Ok(());
 }
