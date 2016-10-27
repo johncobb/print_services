086f76e94a67f4f158dc93bd6a99344fe790cae2
Evan Higgins <evanhiggins@Evans-MacBook-Pro-2.local>
2016-10-27 15:18:20 -0500

    Removed tcp server code.

--- cpprinter_go/main.go
+++ cpprinter_go/main.go
@@ -14,37 +14,6 @@ func main() {
     WriteToSerial(os.Args[1])
 }
 
-//func main() {
-    //listener, err := net.Listen("tcp", "0.0.0.0:5555")
-    //if err != nil {
-        //fmt.Println("Error listening: ", err.Error())
-        //os.Exit(1)
-    //}
-    //defer listener.Close()
-
-    //fmt.Println("Listening on 0.0.0.0:5555")
-    //for {
-        //conn, err := listener.Accept()
-        //if err != nil {
-            //fmt.Println("Error accepting: ", err.Error())
-            //os.Exit(1)
-        //}
-
-        //go handleConnection(conn)
-    //}
-//}
-
-//func handleConnection(conn net.Conn) {
-    //buff := make([]byte, 8192)
-
-    //reqLen, err := conn.Read(buff)
-    //if err != nil {
-        //fmt.Println("Error reading: ", err.Error())
-    //}
-    //WriteToSerial(string(buff[:reqLen]))
-    //conn.Close()
-//}
-
 func WriteToSerial(message string) {
     serialConfig := &serial.Config{Name: "/dev/ttyUSB0", Baud: 9600}
 
