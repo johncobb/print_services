2adee3302da0f266ec5bb80ce8fca930a8c78c4c
Evan Higgins <evanhiggins@Evans-MacBook-Pro-2.local>
2016-10-27 12:57:42 -0500

    Updated cpprinter_go to write to serial

--- cpprinter_go/main.go
+++ cpprinter_go/main.go
@@ -1,43 +1,50 @@
 package main
 
 import (
-    "fmt"
     "os"
-    "net"
     "github.com/tarm/serial"
 )
 
 func main() {
-    listener, err := net.Listen("tcp", "0.0.0.0:5555")
-    if err != nil {
-        fmt.Println("Error listening: ", err.Error())
-        os.Exit(1)
+    if len(os.Args) != 2 {
+        println("Invalid arguments: ", os.Args)
+        os.Exit(-1)
     }
-    defer listener.Close()
 
-    fmt.Println("Listening on 0.0.0.0:5555")
-    for {
-        conn, err := listener.Accept()
-        if err != nil {
-            fmt.Println("Error accepting: ", err.Error())
-            os.Exit(1)
-        }
-
-        go handleConnection(conn)
-    }
+    WriteToSerial(os.Args[1])
 }
 
-func handleConnection(conn net.Conn) {
-    buff := make([]byte, 8192)
+//func main() {
+    //listener, err := net.Listen("tcp", "0.0.0.0:5555")
+    //if err != nil {
+        //fmt.Println("Error listening: ", err.Error())
+        //os.Exit(1)
+    //}
+    //defer listener.Close()
 
-    reqLen, err := conn.Read(buff)
-    if err != nil {
-        fmt.Println("Error reading: ", err.Error())
-    }
-    WriteToSerial(string(buff[:reqLen]))
-    fmt.Println(string(buff[:reqLen]))
-    conn.Close()
-}
+    //fmt.Println("Listening on 0.0.0.0:5555")
+    //for {
+        //conn, err := listener.Accept()
+        //if err != nil {
+            //fmt.Println("Error accepting: ", err.Error())
+            //os.Exit(1)
+        //}
+
+        //go handleConnection(conn)
+    //}
+//}
+
+//func handleConnection(conn net.Conn) {
+    //buff := make([]byte, 8192)
+
+    //reqLen, err := conn.Read(buff)
+    //if err != nil {
+        //fmt.Println("Error reading: ", err.Error())
+    //}
+    //WriteToSerial(string(buff[:reqLen]))
+    //fmt.Println(string(buff[:reqLen]))
+    //conn.Close()
+//}
 
 func WriteToSerial(message string) {
     serialConfig := &serial.Config{Name: "/dev/ttyUSB0", Baud: 9600}
